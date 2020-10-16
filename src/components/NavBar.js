4e723e2a8c6eaa3ee1b01cfab50b0512d4f4fab7
Nguyen Trung Duy <duynt2@toshiba-tsdv.com>
2020-10-16 11:35:37 +0700

    init react context api hooks project

---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+
+class NavBar extends Component {
+    
+    render() {
+        return (
+            <div>
+                <nav>
+                    <h1>Context App</h1>
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
+            </div>
+        )
+    }
+}
+
+export default NavBar
