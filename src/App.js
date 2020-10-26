94c499ca2766f257db5d3a7462142fd6b77e05c2
Nguyen Trung Duy <duynt2@toshiba-tsdv.com>
2020-10-26 17:10:18 +0700

    intro hooks

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
-import BookList from './components/BookList';
-import NavBar from './components/NavBar';
-import ThemeToggle from './components/ThemeToggle';
-import AuthContextProvider from './contexts/AuthContext';
-import ThemeContextProvider from './contexts/ThemeContext';
+// import BookList from './components/BookList';
+// import NavBar from './components/NavBar';
+import SongList from './components/SongList';
+// import ThemeToggle from './components/ThemeToggle';
+// import AuthContextProvider from './contexts/AuthContext';
+// import ThemeContextProvider from './contexts/ThemeContext';
 
 function App() {
   return (
     <div className="App">
-        <ThemeContextProvider>
-          <AuthContextProvider>
-            <NavBar />
-            <BookList />
-            <ThemeToggle />
-          </AuthContextProvider>
-        </ThemeContextProvider>
+        <SongList />
     </div>
   );
 }
